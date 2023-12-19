1e00ed890a2dd3389e6ab12377a48d5be6d6407a
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-19 23:00:26 +0100

    added form and table

--- js/script.js
+++ js/script.js
@@ -1,34 +1,24 @@
 console.log('JS OK')
 // INITIAL VARIABLES
+// Prendiamo l'elemento button
 const button = document.getElementById('button')
+
+// Prendiamo l'elemento input
 const input = document.getElementById('kilometers')
+
+// Prendiamo l'elemento select
 const select = document.getElementById('età')
-document.getElementById=('paragraph')
+
+// Prendiamo l'elemento paragraph
+const paragraph = document.getElementById('paragraph');
+
+// Iniziamo a creare il messaggio
 const message=('Il prezzo del biglietto è : ')
 
-//MAIN LOGICS
-button.addEventListener('click', function(){
- console.log('kilometri percorsi',parseInt(input.value))
- console.log('età', select.value)
- console.log('Prezzo biglietto', basePrice)
- console.log('Prezzo biglietto minorenni', ticketPriceMinors.toFixed(2))
- console.log('Prezzo biglietto over65', ticketPriceOver65.toFixed(2))
-    if(select.value == 'Minorenne'){
-        paragraph.innerText= `${message} ${ticketPriceMinors.toFixed(2)}€`
-        console.log(paragraph)
-    }
-    else if(select.value == 'Over 65'){
-        paragraph.innerText= `${message} ${ticketPriceOver65.toFixed(2)}€`
-        console.log(paragraph)
-    }
-    else{
-        paragraph.innerText= `${message} + ${basePrice}`
-        console.log(paragraph)
-    }
-});
-
-// Calcoliamo il prezzo del biglietto//
+// Prezzo al chilometro
 const kilometersPrice = 0.21
+
+// Calcoliamo il prezzo base
 const basePrice = kilometersPrice * input.value
 
 // Calcoliamo il prezzo del biglietto per i minorenni//
@@ -37,20 +27,27 @@ const ticketPriceMinors = basePrice - (basePrice / 100 * 20);
 // Calcoliamo il prezzo del biglietto per gli over 65//
 const ticketPriceOver65= basePrice - (basePrice / 100 * 40);
 
-// Prendiamo l'elemento nell'html//
-document.getElementById=('paragraph')
-// paragraph.innerText= `${message} + ${basePrice}`
 
-// Costruisco il messaggio passeggeri compresi tra i 18 e i 65 anni di età//
-// const message=('Il prezzo del biglietto è : ')
-// paragraph.innerText= `${message} ${basePrice.toFixed(2)}€`
-// Messaggio per passeggeri minorenni//
-// if(select.value < 18){
-//     paragraph.innerText= `${message} ${ticketPriceMinors.toFixed(2)}€`
-//     console.log(paragraph)
-// }
-// Messaggio per passeggeri over 65//
-// else if(select.value > 65){
-//     paragraph.innerText= `${message} ${ticketPriceOver65.toFixed(2)}€`
-//     console.log(paragraph)
-// }
+//MAIN LOGICS
+button.addEventListener('click', function(){
+ console.log('kilometri percorsi',parseInt(input.value))
+ console.log('età', select.value)
+ console.log('Prezzo biglietto', basePrice)
+ console.log('Prezzo biglietto minorenni', ticketPriceMinors.toFixed(2))
+ console.log('Prezzo biglietto over65', ticketPriceOver65.toFixed(2))
+
+    if(select.value == 'Minorenne'){
+        paragraph.innerText = `${message} ${ticketPriceMinors.toFixed(2)}€`
+        console.log(paragraph)
+    }
+    else if(select.value == 'Over 65'){
+        paragraph.innerText = `${message} ${ticketPriceOver65.toFixed(2)}€`
+        console.log(paragraph)
+    }
+    else{
+        paragraph.innerText = `${message} + ${basePrice}`
+        console.log(paragraph)
+    }
+});
+
+
